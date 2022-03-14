1c63959c49b60061cb08ad4d0b4a067d7ad3e335
meixinchoy <65379600+meixinchoy@users.noreply.github.com>
2022-03-15 07:25:22 +0800

    Update symmetric_tree.js

--- Week7/symmetric_tree.js
+++ Week7/symmetric_tree.js
@@ -1,1 +1,20 @@
 
+//https://leetcode.com/problems/symmetric-tree/
+
+var isSymmetric = function(root) {
+    if(!root)return true;
+    
+    return helper(root.left, root.right);
+};
+
+var helper = (leftNode, rightNode) => {
+    if(!leftNode && !rightNode){
+        return true;
+    }else if(!leftNode || !rightNode || leftNode.val!==rightNode.val){
+        return false;
+    }
+    
+    //recursively check for subtrees
+    return helper(leftNode.left,rightNode.right) && helper(leftNode.right,rightNode.left);
+    
+}
